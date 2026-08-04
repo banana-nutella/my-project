611c8e316a326e01fe816a09ae99dd25632d8211
banana-nutella <hugo.chattra@gmail.com>
2026-08-04 10:21:45 +0700

    restored simplify.js

---
+++ api/simplify.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+    const userText = req.body.text;
+
+    const response = await fetch("https://api.anthropic.com/v1/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.CLAUDE_API_KEY,
+          "anthropic-version": "2023-06-01"
+        },
+        body: JSON.stringify({
+            model: "claude-haiku-4-5-20251001",
+            max_tokens: 1000,
+            messages: [
+                {role: "user", content: `Simplify this text to a 4th grade reading level: ${userText}`}
+            ]
+        })})
+    const data = await response.json();
+    const simplifiedText = data.content[0].text;
+
+    res.status(200).json({simplifiedText: simplifiedText});
+}
